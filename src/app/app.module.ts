e2d90906b32e42c6c08a9bbed38ac027e19d588b
shyd82 <shyd82@hotmail.com>
2020-03-12 11:15:19 -0400

    Create Component and Property and Methods

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { Server } from 'http';
-import {ServerComponent} from './server/server.component'
+// import { Server } from 'http';
+import {ServerComponent} from './server/server.component';
 
 @NgModule({
   declarations: [
--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -2,12 +2,41 @@ import {Component} from '@angular/core';
 
 @Component ({
     selector:'app-server',
-    // templateUrl:'./server/server.component.html'
-    template:'<h2>hello Angular welcome Di</h2>'
+    templateUrl:'./server.component.html',
+    // template:'<h2>hello Angular welcome Di</h2>',
+    styleUrls:['./server.component.css']
     
 })
 
 export class ServerComponent {
+
+    // Property
+    firstName = 'Di';
+    lastName = 'Ye';
+    age ='32';
+
+
+    // Methods:
+    constructor(){
+        // console.log('hello server component');
+
+        this.sayHello();
+       
+        // this.hasBirthday();
+        // console.log(this.age);
+
+    }
+
+    sayHello(){
+        // console.log('Hello ${this.firstName}');
+        console.log('Hello' + ' ' + this.firstName);
+
+    }
+
+    hasBirthday(){
+        // this.age == this.age+1;
+        // this.age += 1;
+    }
 
 }
 
