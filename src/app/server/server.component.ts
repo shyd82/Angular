7c5d7193c2719e4da631453e2f10ea9c321ad0be
shyd82 <shyd82@hotmail.com>
2020-03-12 01:36:17 -0400

    first Angular code

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,14 @@
+import {Component} from '@angular/core';
+
+@Component ({
+    selector:'app-server',
+    // templateUrl:'./server/server.component.html'
+    template:'<h2>hello Angular welcome Di</h2>'
+    
+})
+
+export class ServerComponent {
+
+}
+
+
